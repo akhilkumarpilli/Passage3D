8ba52af7b99cb321daad1e23fabe1da841a2f493
akhilkumarpilli <akhilkumar7947@gmail.com>
2025-03-19 15:59:09 +0530

    update records for testing

--- app/upgrades/v2.5.0/migrations.go
+++ app/upgrades/v2.5.0/migrations.go
@@ -3,22 +3,18 @@ package v2_5
 var AddressMigrations = []AddressMigration{
 	{
 		OldAddress: "pasg19zkz9x0u84a0ykzm8lggwy5x0s0gcnwskark5c",
-		NewAddress: "",
+		NewAddress: "pasg1un7pragk9r924lvu96z5r3wat0qhrdmtx6swfx",
 	},
 	{
 		OldAddress: "pasg1sap5junfzydgqcll4ezyl4sh4yeekl64qkna34",
-		NewAddress: "",
-	},
-	{
-		OldAddress: "pasg1lel0s624jr9zsz4ml6yv9e5r4uzukfs7hwh22w",
-		NewAddress: "",
-	},
-	{
-		OldAddress: "pasg1vl7u3a9p37ajemv7wyvuegh7mhujtmdvpt8apu",
-		NewAddress: "",
+		NewAddress: "pasg1thjmz9yjsuz52ny2zcs50c7t2gs8g4xx2uhm6k",
 	},
 	{
 		OldAddress: "pasg1pfdyn3nzajult9e6s2nvhmlgaeglh8lljdv779",
-		NewAddress: "",
+		NewAddress: "pasg1hrsrenuq67jdjh4lsk4ne6ne0nrh52t8esallr",
+	},
+	{
+		OldAddress: "pasg1vl7u3a9p37ajemv7wyvuegh7mhujtmdvpt8apu",
+		NewAddress: "pasg1n4ke7uqmgwcqt6w83e8n6sy2y6ctxmxr534smc",
 	},
 }
